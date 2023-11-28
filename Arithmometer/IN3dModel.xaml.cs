f91ae2da08814cef21050394071c46f134f4cb68
Kirill <kiry.evstigneev@gmail.com>
2023-11-28 22:06:53 +0300

    3слайд

---
+++ Arithmometer/IN3dModel.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Arithmometer
+{
+    /// <summary>
+    /// Логика взаимодействия для IN3dModel.xaml
+    /// </summary>
+    public partial class IN3dModel : Window
+    {
+        public IN3dModel()
+        {
+            InitializeComponent();
+        }
+    }
+}
