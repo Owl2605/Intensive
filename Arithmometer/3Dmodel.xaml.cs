d25d05405f1f0ea24697afc008c089d9139df0a6
oleshkasok <oleshkasok@mail.ru>
2023-12-12 16:21:22 +0300

    Добавил серверную часть в 3Dmodel

--- Arithmometer/3Dmodel.xaml.cs
+++ Arithmometer/3Dmodel.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.IO;
 using System.Reflection;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
 
 namespace Arithmometer
 {
@@ -37,7 +40,7 @@ namespace Arithmometer
         public _3Dmodel()
         {
             InitializeComponent();
-
+            Start();
             ModelVisual3D device3D = new ModelVisual3D();
             Model3D model3D = Display3d(MODEL_PATH);
             ScaleTransform3D _zScaleTransform = new ScaleTransform3D();
@@ -75,6 +78,76 @@ namespace Arithmometer
                 MessageBox.Show("Exception Error : " + e.StackTrace);
             }
             return device;
+        }
+
+        Thread thread;
+        public int connectionPort = 25001;
+        TcpListener server;
+        TcpClient client;
+        bool running;
+        void Start()
+        {
+            ThreadStart ts = new ThreadStart(GetData);
+            thread = new Thread(ts);
+            thread.Start();
+        }
+
+        void GetData()
+        {
+            try
+            {
+                server = new TcpListener(IPAddress.Any, connectionPort);
+                server.Start();
+                Console.WriteLine("Сервер готов к получению клиента");
+                client = server.AcceptTcpClient();
+                Console.WriteLine("Клиент подключился к серверу");
+                running = true;
+                //NetworkStream nwStream = client.GetStream();
+                //byte[] buffer = new byte[64];
+                //int bytesRead = nwStream.Read(buffer, 0, buffer.Length);
+                //Console.WriteLine("test1 " + bytesRead);
+                while (running)
+                {
+                    Connection();
+                }
+                server.Stop();
+            }
+            catch { }
+        }
+
+        void Connection()
+        {
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+            string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            if (dataReceived.Length != 0)
+            {
+                Console.WriteLine(dataReceived);
+                doIt(dataReceived);
+            }
+            //string s = "true";
+            //byte[] bytes = Encoding.UTF8.GetBytes(s);
+            //Console.WriteLine("true");
+            //nwStream.Write(bytes, 0, bytes.Length);
+
+        }
+        void doIt(string str)
+        {
+            if (str.Length == 2) // Если длина строки равна двум, значит мы передали на сервер данные по рычажку
+                                 //первая цифра - номер рычажка, вторая цифра - цифра на рычажке
+            {
+                //тут должен быть код, связанный с выставлением цифр на рычажках 
+                //на рычажке str[0] выставить значение str[1]
+            }
+            if (str == "вращаем ручку") // выполнить код для вращения ручки
+            {
+
+            }
+            if (str.Length == 1) //выставить каретку на значение str[0]
+            {
+
+            }
         }
     }
 }
